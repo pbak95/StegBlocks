7a787f32591cee45504921ab102e7b048c9e89c9
pbak95 <patryk.bak@gazeta.pl>
2018-03-15 00:37:07 +0100

    Initial test commit

---
+++ StegBlocks-Client/src/main/java/HelloWorldClient.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Patryk on 15.03.2018.
+ */
+public class HelloWorldClient {
+
+    public static void main(String args[]) {
+        System.out.println("This is client.");
+    }
+}
---
+++ StegBlocks-Server/src/main/java/HelloWorldServer.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Patryk on 15.03.2018.
+ */
+public class HelloWorldServer {
+
+    public static void main(String args[]) {
+        System.out.println("Hello from server.");
+    }
+}
