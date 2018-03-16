f9c74502739bd16fb23eb8a89a422b11c7a1d568
michakrzeminski <micha.krzeminski@gmail.com>
2018-03-16 17:33:04 +0100

    Example for SCTP oracle library

--- StegBlocks-Client/src/main/java/HelloWorldClient.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Patryk on 15.03.2018.
- */
-public class HelloWorldClient {
-
-    public static void main(String args[]) {
-        System.out.println("This is client.");
-    }
-}
--- StegBlocks-Server/src/main/java/HelloWorldServer.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Patryk on 15.03.2018.
- */
-public class HelloWorldServer {
-
-    public static void main(String args[]) {
-        System.out.println("Hello from server.");
-    }
-}
