1b25800d3db9cb30ae7b11d1612500d3477a24a6
pbak95 <patryk.bak@gazeta.pl>
2018-04-17 20:07:56 -0400

    Upgrade arguments handling and also add new arguments, add parametrized network scanner, add network mock for scanner purposes

--- StegBlocks-Client_v2/src/main/java/Client_TCP_v2.java
+++ StegBlocks-Client_v2/src/main/java/Client_TCP_v2.java
@@ -10,19 +10,20 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.Inet4Address;
+import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.nio.ByteBuffer;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.*;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class Client_TCP_v2 implements Runnable {
 
-    private static final String FILE_BEFORE = "/home/michal/Desktop/BEST/StegBlocks/test.txt";
-    private static final String FILE_AFTER = "/home/michal/Desktop/BEST/StegBlocks/test-encoded.txt";
+    private static final String FILE_BEFORE = "/home/osboxes/Studia/BEST/projekt/StegBlocks/test.txt";
+    private static final String FILE_AFTER = "/home/osboxes/Studia/BEST/projekt/StegBlocks/test-encoded.txt";
     private static final int START_PORT = 122;    //could be changed later to spread ports across free space
     private static final int END_PORT = 150;    //could be changed later to spread ports across free space
     private static final int CONNECTIONS_NUMBER = 10; //number of ports used to transmit characters
@@ -30,23 +31,43 @@ public class Client_TCP_v2 implements Runnable {
     private static final DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS");
     private static ArrayList<Integer> portmap;
 
-    private static final String HOST = "10.0.2.15";
-
-    private boolean transmission = false;
+    private String targetHost;
+    private String currentHost;
+    private boolean transmission;
+    private boolean isScanOptionEnabled;
+    private int scanPeriod;
+    private int scanTimeUnit;
     private ByteBuffer filebuf;
     private List<PcapIf> alldevs; // Will be filled with NICs
     private StringBuilder errbuf; // For any error msgs
     private Pcap pcap; //sniffer
     private PcapPacketHandler<String> jpacketHandler; //packet handler
     private List<PcapPacket> tcpPacketPool;
+    private List<String> hostsInNetwork;
+    ScheduledExecutorService executor; //executor for scanning task
 
 
 
-    public Client_TCP_v2(boolean transmission) {
+    public Client_TCP_v2(String targetHost, String currentHost, boolean transmission) {
         this.transmission = transmission;
-        tcpPacketPool = new ArrayList<>();
+        this.tcpPacketPool = new ArrayList<>();
         this.alldevs = new ArrayList<PcapIf>();
         this.errbuf = new StringBuilder();
+        this.hostsInNetwork = Collections.synchronizedList(new ArrayList<>());
+        this.targetHost = targetHost;
+        this.currentHost = currentHost;
+    }
+
+    public Client_TCP_v2(String targetHost, String currentHost,  boolean transmission, boolean isScanOptionEnabled,
+                         int scanPeriod, int scanTimeUnit) {
+        this(targetHost, currentHost, transmission);
+        this.isScanOptionEnabled = isScanOptionEnabled;
+        this.scanPeriod = scanPeriod;
+        this.scanTimeUnit = scanTimeUnit;
+        if (isScanOptionEnabled)
+            runHostDiscoveryTask();
+        else
+            mockHostDiscoveryTask();
     }
 
     @Override
@@ -61,6 +82,7 @@ public class Client_TCP_v2 implements Runnable {
             } else {
                 sendSomeTraffic();
             }
+            executor.shutdown();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -135,10 +157,12 @@ public class Client_TCP_v2 implements Runnable {
         Ip4 ip = packetToOverwrite.getHeader(new Ip4());
         Tcp tcp = packetToOverwrite.getHeader(new Tcp());
         tcp.destination(port);
-        ip.destination(Inet4Address.getByName(HOST).getAddress());
+        ip.destination(Inet4Address.getByName(targetHost).getAddress());
+        ip.source(getHostFromNetwork());
         //Recalculate packet checksum after overwrite
         ip.checksum(ip.calculateChecksum());
         tcp.checksum(tcp.calculateChecksum());
+        //System.out.println(packetToOverwrite);
         if (pcap.sendPacket(packetToOverwrite) != Pcap.OK) {
             System.err.println(pcap.getErr() + " Packet size: " + packetToOverwrite.size());
         }
@@ -176,6 +200,7 @@ public class Client_TCP_v2 implements Runnable {
                     Save tcp packets
                  */
                 if (packet.hasHeader(tcp)) {
+                    if (tcp.size() < 1800)
                     tcpPacketPool.add(packet);
                 }
             }
@@ -242,17 +267,104 @@ public class Client_TCP_v2 implements Runnable {
         is.close();
     }
 
+    private void runHostDiscoveryTask() {
+        hostsInNetwork.add(this.currentHost);
+        TimeUnit timeUnit;
+
+        Runnable scanTask = () -> {
+            String host="10.0.2" + "." +
+                    Client_TCP_v2.randomNumberInRange(2,244);
+            try {
+                if (InetAddress.getByName(host).isReachable(1000)){
+                    hostsInNetwork.add(host);
+                    logMessage("New host discovered: " + host);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        };
+
+        if (scanTimeUnit == 1) {
+            timeUnit = TimeUnit.MINUTES;
+        } else {
+            timeUnit = TimeUnit.SECONDS;
+        }
+
+        this.executor = Executors.newScheduledThreadPool(1);
+        this.executor.scheduleAtFixedRate(scanTask, 0, scanPeriod, timeUnit);
+    }
+
+    private void mockHostDiscoveryTask() {
+        String[] ipParts = this.currentHost.split("\\.");
+        StringBuilder networkPrefix = new StringBuilder();
+        networkPrefix.append(ipParts[0]);
+        networkPrefix.append(".");
+        networkPrefix.append(ipParts[1]);
+        networkPrefix.append(".");
+        networkPrefix.append(ipParts[2]);
+        networkPrefix.append(".");
+        String prefix = networkPrefix.toString();
+        for (int i=1; i < 255; i++) {
+            hostsInNetwork.add(prefix + i);
+        }
+    }
+
+    private byte[] getHostFromNetwork() throws UnknownHostException {
+        synchronized (hostsInNetwork) {
+            return Inet4Address.getByName(hostsInNetwork.get(randomNumberInRange(0, hostsInNetwork.size() - 1))).getAddress();
+        }
+    }
+
     private void logMessage(String message) {
         System.out.println("[" + LocalDateTime.now().format(format) + "] " + message);
     }
 
+    public static int randomNumberInRange(int min, int max) {
+        Random random = new Random();
+        return random.nextInt((max - min) + 1) + min;
+    }
+
+    public static void handleMain(String[] args) {
+        boolean transmission;
+        boolean isScanOptionEnabled = false;
+        int scanPeriod = 0;
+        int scanTimeUnit = 0;
+
+        if (args[2].equals("true") && args[2] != null) {
+            transmission = true;
+        } else {
+            transmission = false;
+        }
+
+        if (args.length > 3) {
+
+         if (args[3].equals("true")) {
+            isScanOptionEnabled = true;
+            scanPeriod = Integer.parseInt(args[4]);
+            scanTimeUnit = Integer.parseInt(args[5]);
+         } else {
+             isScanOptionEnabled = false;
+         }
+
+        }
+
+        if (args.length > 3) {
+            new Client_TCP_v2(args[0], args[1], transmission, isScanOptionEnabled, scanPeriod, scanTimeUnit).run();
+        } else {
+            new Client_TCP_v2(args[0], args[1], transmission).run();
+        }
+
+    }
+
     public static void main(String[] args) {
-        if(args[0].equals("true")) {
-            new Client_TCP_v2(true).run();
-        }
-        else {
-            new Client_TCP_v2(false).run();
-        }
+
+        Client_TCP_v2.handleMain(args);
+
+//        if(args[0].equals("true"))
+//            new Client_TCP_v2(true).run();
+//
+//        else
+//            new Client_TCP_v2(false).run();
     }
 
 }
